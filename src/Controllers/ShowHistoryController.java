40352aa34e60b6986392b9347172a16c2375b55d
Joshua vayle <josh75337@gmail.com>
2018-12-02 14:29:17 -0500

    Fixed show history to use SQL for the joining

--- src/Controllers/ShowHistoryController.java
+++ src/Controllers/ShowHistoryController.java
@@ -20,43 +20,56 @@ public class ShowHistoryController {
         try {
             Statement statement = conn.createStatement();
             String sql = "SELECT * FROM VisitShow WHERE VisitorUsername=\"" + username + "\"";
-            statement.execute(sql);
+            String otherSQL = "SELECT Name, LocatedIn FROM Shows GROUP BY Name, LocatedIn";
+            String finalSQL = "Select * from (" + sql +") t1 INNER JOIN ( " + otherSQL + " ) t2 ON t1.ShowName=t2.Name";
+
+
+            statement.execute(finalSQL);
             set = statement.getResultSet();
-            while(set.next()) {
-                String visitorUsername = set.getString(1);
-                String showName = set.getString(2);
-                String showTime = set.getString(3);
-                HashMap<String, String> exhibitName = getExhibitName();
-                visitShow = new VisitShow(visitorUsername, showName, showTime, exhibitName.get(showName));
+
+            while(set.next()){
+                String name = set.getString(1);
+                String show = set.getString(2);
+                String time = set.getString(3);
+                String exhibit = set.getString(5);
+                visitShow = new VisitShow(name,show,time, exhibit);
                 list.add(visitShow);
             }
+//            while(set.next()) {
+//                String visitorUsername = set.getString(1);
+//                String showName = set.getString(2);
+//                String showTime = set.getString(3);
+//                //HashMap<String, String> exhibitName = getExhibitName();
+//                visitShow = new VisitShow(visitorUsername, showName, showTime, exhibitName.get(showName));
+//                list.add(visitShow);
+//            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return list;
     }
 
-    public HashMap<String, String> getExhibitName() {
-        ResultSet set;
-        String name;
-        String locatedIn;
-        HashMap<String, String> list = new HashMap<>();
-        Connection conn = DatabaseConnector.establishConnection();
-        try {
-            Statement statement = conn.createStatement();
-            String sql = "SELECT Name, LocatedIn FROM Shows GROUP BY Name, LocatedIn";
-            statement.execute(sql);
-            set = statement.getResultSet();
-            while(set.next()) {
-                name = set.getString(1);
-                locatedIn = set.getString(2);
-                list.put(name, locatedIn);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
+//    public HashMap<String, String> getExhibitName() {
+//        ResultSet set;
+//        String name;
+//        String locatedIn;
+//        HashMap<String, String> list = new HashMap<>();
+//        Connection conn = DatabaseConnector.establishConnection();
+//        try {
+//            Statement statement = conn.createStatement();
+//            String sql = "SELECT Name, LocatedIn FROM Shows GROUP BY Name, LocatedIn";
+//            statement.execute(sql);
+//            set = statement.getResultSet();
+//            while(set.next()) {
+//                name = set.getString(1);
+//                locatedIn = set.getString(2);
+//                list.put(name, locatedIn);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return list;
+//    }
 
     public void insertVisit(String username, String showName, String dateTime) {
         Connection conn = DatabaseConnector.establishConnection();
@@ -77,23 +90,35 @@ public class ShowHistoryController {
         Connection conn = DatabaseConnector.establishConnection();
         try {
             Statement statement = conn.createStatement();
-            HashMap<String, String> exhibitNames = getExhibitName();
+            //HashMap<String, String> exhibitNames = getExhibitName();
             String sql = "SELECT * FROM VisitShow WHERE (VisitorUsername=\"" + username + "\" OR \"" + username + "\"=\"\")"
                          + " AND (ShowName=\"" + showName + "\" OR \"" + showName + "\"=\"\")"
                          + " AND (ShowTime=\"" + showTime + "\" OR \"" + showTime + "\"=\"\")"
                          + " ORDER BY " + orderColumn + " " + orderType;
 //                         + " AND (" + exhibit + ") = \"" + exhibitName + "\"";
-            statement.execute(sql);
+            String otherSQL = "SELECT Name, LocatedIn FROM Shows GROUP BY Name, LocatedIn";
+            String finalSQL = "Select * from (" + sql +") t1 INNER JOIN ( " + otherSQL + " ) t2 ON t1.ShowName=t2.Name";
+
+            statement.execute(finalSQL);
+            //statement.execute(sql);
             set = statement.getResultSet();
-            while(set.next()) {
+//            while(set.next()) {
+//                String name = set.getString(1);
+//                String show = set.getString(2);
+//                String time = set.getString(3);
+//                String exhibit = exhibitNames.get(show);
+//                if (exhibit.equals(exhibitName)) {
+//                    visitShow = new VisitShow(name, show, time, exhibitNames.get(show));
+//                    list.add(visitShow);
+//                }
+//            }
+            while(set.next()){
                 String name = set.getString(1);
                 String show = set.getString(2);
                 String time = set.getString(3);
-                String exhibit = exhibitNames.get(show);
-                if (exhibit.equals(exhibitName)) {
-                    visitShow = new VisitShow(name, show, time, exhibitNames.get(show));
-                    list.add(visitShow);
-                }
+                String exhibit = set.getString(5);
+                visitShow = new VisitShow(name,show,time, exhibit);
+                list.add(visitShow);
             }
         } catch (SQLException e) {
             e.printStackTrace();
